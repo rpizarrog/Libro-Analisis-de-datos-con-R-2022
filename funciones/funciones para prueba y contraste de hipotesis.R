7773b7d70e2edeb3eb83fa79d76260dd9ca23745
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2023-09-05 08:49:46 -0600

    Add files via upload

--- funciones/funciones para prueba y contraste de hipotesis.R
+++ funciones/funciones para prueba y contraste de hipotesis.R
@@ -75,10 +75,90 @@ f_probar_hipotesis_all <- function (h0, h1, tipo = 1, h0_string ="Realidad actua
 # Recibe la media de la muestra, el valor de la media pobacoinal a comparar,
 # la desviación estándar conocida de la población y e valor de n tamaño de la muestra.
 # Devuelve el valor de z.
-f.devolver.z.prueba <- function(media_m, desv_std, media_p, n) {
+f_devolver_z_prueba <- function(media_m, desv_std, media_p, n) {
   z <- (media_m - media_p) / (desv_std / sqrt(n))
   z
 }
+
+# Septiembre 2023
+# Recibe los parámetros del de confianza en valores relativos 
+# entre 0 y 1, por ejemplo 0.90, 0.95, 0.99; 
+# recibe el valor de z previamene calculado con la función f_devolver_z_prueba()
+# # recibe la expresión de una hipótesis en caso de que no se proporcione
+# la expresión por omisión en la llamada de la función de la hipótesis nula es 
+# "Realidad actual verdadera"
+# recibe el parámetro de cola dependiendo el tipo de hipótesis a tratar:
+# a dos colas = 1, cola izquierda = 2, cola derecha = 3, 
+# por definición default es a dos colas 
+# Devuelve una lista con la gráfica de aceptación o rechazo de Ho y la decisión de Ho
+f_probar_hipotesis_z <- function(confianza, z, h0_string ="Realidad actual verdadera", cola = 1) {
+  library(visualize)
+  
+  H0 <- NULL
+  if (cola == 1)  { 
+    # Dos colas
+    alfa <- (1 - confianza) / 2
+    z.critico <- qnorm(p = alfa)
+    z.critico <- abs(z.critico)
+    z.critico
+    
+    tipo_cola <- "dos colas"
+    g <- visualize.norm(stat = c(-z.critico, z.critico), section = "tails") +
+      text(0, 0.2, paste(tipo_cola, "\n", confianza * 100, "%", "\n", 
+                         "alfa=", (1 - confianza), "\n",  "alfa / 2 = ", 
+                         (1 - confianza) /  2, "\n", "Acepta Ho", sep = ""),  col = "black") + 
+      abline(v = z, col='red', lwd = 1, lty= 4)
+    
+    if (z > -z.critico & z < z.critico) {
+      H0 <- paste("Se acepta la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    } else {
+      H0 <- paste("Se rechaza la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    }
+      
+  }
+  if (cola == 2)  { 
+    # Cola a la derecha
+    alfa <- (1 - confianza) 
+    z.critico <- qnorm(p = alfa)
+    z.critico
+    
+    tipo_cola <- "cola a la izquierda"
+    g <- visualize.norm(stat = c(z.critico), section = "lower") +
+      text(0, 0.2, paste(tipo_cola, "\n", confianza * 100, "%", "\n", 
+                         "alfa=", (1 - confianza), "\n",  "alfa / 2 = ", 
+                         (1 - confianza) /  2, "\n", "Acepta Ho", sep = ""),  col = "black") + 
+      abline(v = z, col='red', lwd = 1, lty= 4)
+    if (z > z.critico ) {
+      H0 <- paste("Se acepta la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    } else {
+      H0 <- paste("Se rechaza la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    }
+  }
+  if (cola == 3)  { 
+    # Cola a la derecha
+    alfa <- (1 - confianza) 
+    z.critico <- qnorm(p = alfa, lower.tail = FALSE)
+    z.critico
+    
+    tipo_cola <- "cola a la derecha"
+    g <- visualize.norm(stat = c(z.critico), section = "upper") +
+      text(0, 0.2, paste(tipo_cola, "\n", confianza * 100, "%", "\n", 
+                         "alfa=", (1 - confianza), "\n",  "alfa / 2 = ", 
+                         (1 - confianza) /  2, "\n", "Acepta Ho", sep = ""),  col = "black") + 
+      abline(v = z, col='red', lwd = 1, lty= 4)
+    
+    if (z < z.critico ) {
+      H0 <- paste("Se acepta la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    } else {
+      H0 <- paste("Se rechaza la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    }
+  }
+  
+  lista <- list(g = g, H0 = H0)
+  return(lista)
+}
+
+
 
 
 # Septiembre 2023
