a9569ed878960313e3fbee75e55fa7ed207a5459
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2023-09-15 08:45:10 -0600

    Add files via upload

--- funciones/funciones para prueba y contraste de hipotesis.R
+++ funciones/funciones para prueba y contraste de hipotesis.R
@@ -309,6 +309,7 @@ f_probar_hipotesis_t <- function(confianza, t, h0_string ="Realidad actual verda
 # cola a la izquierda el valor es 2 y cola a la derecha el valor es 3. 
 # Por omisión, el valor por default es a dos colas = 1
 # Recibe el valor de n que define los grados de libertad
+
 f_probar_hipotesis_p_t <- function(t, significancia, cola=1, n) {
   decision <- "Se acepta Ho"
   rechaza <- NULL
@@ -350,5 +351,287 @@ f_probar_hipotesis_p_t <- function(t, significancia, cola=1, n) {
   
   return(lista)
 }
+
+
+# Septiembre 2023
+# La función f_devolver_chisq_prueba recibe los parámetros de acuerdo a la fórmula y devuelve 
+# el valor de chi cuadrada de prueba para su contraste en 
+# prueba de hipótesis de variabilidad de los datos
+# Recibe la varianza de la muestra,
+# la varianza de la población o valor hipótetico a comparar en la H0, 
+# de n tamaño de la muestra para calcular los grados de libertad
+# Devuelve el valor de prueba de chi cuadrada en p_chisq.
+f_devolver_chisq_prueba <- function(var_m, var_p, n) {
+  p_chisq <- (gl * var_m) / var_a_comparar
+  p_chisq
+}
+
+
+
+# Septiembre 2023
+# La función f_probar_hipotesis_chisq()
+# Recibe los parámetros del de confianza en valores relativos 
+# entre 0 y 1, por ejemplo 0.90, 0.95, 0.99; 
+# recibe el valor de p_chisq previamente calculado con la función f_devolver_chisq_prueba()
+# recibe la expresión de una hipótesis en caso de que no se proporcione
+# la expresión por omisión en la llamada de la función de la hipótesis nula es 
+# "Realidad actual verdadera"
+# recibe el parámetro de cola dependiendo el tipo de hipótesis a tratar:
+# a dos colas = 1, cola izquierda = 2, cola derecha = 3, 
+# por definición default es a dos colas, 
+# finalmente recibe el valor de n que define los grados de libertad 
+# La función utiliza visualize.chisq() para representar el valor de p_chisq y los valores críticos
+# Devuelve una lista con la gráfica de aceptación o rechazo de Ho y la decisión de Ho
+f_probar_hipotesis_chisq <- function(confianza, p_chisq, h0_string ="Realidad actual verdadera", cola = 1, n) {
+  library(visualize)
+  
+  H0 <- NULL
+  gl <- (n - 1)
+  if (cola == 1) {
+    alfa <- (1 - confianza) 
+    chisq_critico_izq = qchisq(p = alfa/2, df = gl)
+    chisq_critico_der = qchisq(p = 1 - alfa/2, df = gl)
+    
+    # Visualizar
+    tipo_cola <- "dos colas"
+    g <- visualize.chisq(stat = c(chisq_critico_izq, chisq_critico_der), df = gl, section = "tails") +
+      text(0, 0.2, paste(tipo_cola, "\n", confianza * 100, "%", "\n", 
+                         "alfa=", (1 - confianza), "\n",  "alfa / 2 = ", 
+                         (1 - confianza) /  2, "\n", "Acepta Ho", sep = ""),  col = "black") + 
+      abline(v = p_chisq, col='red', lwd = 1, lty= 4)  
+    
+    if (p_chisq > -chisq_critico_izq & p_chisq < chisq_critico_der) {
+      H0 <- paste("Se acepta la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    } else {
+      H0 <- paste("Se rechaza la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    }
+    
+  }
+  if (cola == 2) {
+    alfa <- (1 - confianza) 
+    tipo_cola <- "cola izquierda"
+    chisq_critico = qchisq(p = alfa, df = gl)
+    
+    # Visualizar
+    g <- visualize.chisq(stat = c(p_chisq), df = gl, section = "lower") +
+      text(0, 0.2, paste(tipo_cola, "\n", confianza * 100, "%", "\n", 
+                         "alfa=", (1 - confianza), "Acepta Ho", sep = ""),  col = "black") + 
+      abline(v = chisq_critico, col='red', lwd = 1, lty= 4)  
+    
+    if (p_chisq > chisq_critico) {
+      H0 <- paste("Se acepta la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    } else {
+      H0 <- paste("Se rechaza la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    }
+  }
+  if (cola == 3) {
+    alfa <- (1 - confianza)
+    tipo_cola <- "cola derecha"
+    chisq_critico = qchisq(p = alfa, df = gl, lower.tail = FALSE)
+
+    # Visualizar
+    g <- visualize.chisq(stat = c(p_chisq), df = gl, section = "upper") +
+      text(0, 0.2, paste(tipo_cola, "\n", confianza * 100, "%", "\n", 
+                         "alfa=", (1 - confianza), "Acepta Ho", sep = ""),  col = "black") + 
+      abline(v = chisq_critico, col='red', lwd = 1, lty= 4) 
+    if (p_chisq < chisq_critico) {
+      H0 <- paste("Se acepta la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    } else {
+      H0 <- paste("Se rechaza la hipótesis nula Ho:",h0_string,"con nivel de confianza", confianza*100,"%", "a",tipo_cola)
+    }
+    
+  }
+  
+  lista <- list(g = g, H0 = H0)
+  return(lista)
+}
+
+
+# Septiembre 2023
+# La función f_probar_hipotesis_p_chisq() devuelve la decisión de aceptar o rechazar la hipótesis nula
+# basado en la comparación de el valor de prueba chisq contra el valor de significncia
+# Recibe los parámetros de p_chisq obtenido a partir de función f_devolver_chisq_prueba(), 
+# recibe el valor de significancia que por default es 0.05, pero pudiera ser 0.10, 0.01, 0.001 u otro y 
+# recibe el valor del tipo de hipótesis, si es a dos colas el valor es 1, 
+# cola a la izquierda el valor es 2 y cola a la derecha el valor es 3. 
+# Por omisión, el valor por default es a dos colas = 1
+# Recibe el valor de n que define los grados de libertad
+
+f_probar_hipotesis_p_chisq <- function(p_chisq, significancia, cola=1, n) {
+  decision <- "Se acepta Ho"
+  rechaza <- NULL
+  gl <- (n-1)
+  if (isTRUE(all.equal(significancia, 0.10))) {
+    rechaza <- "hay cierta evidencia de que H0 no es verdadera"
+  }
+  if (isTRUE(all.equal(significancia, 0.05))) {
+    rechaza <- "hay evidencia fuerte de que H0 no es verdadera"
+  }
+  if (isTRUE(all.equal(significancia, 0.01))) {
+    rechaza <- "hay evidencia muy fuerte de que H0 no es verdadera"
+  }
+  if (isTRUE(all.equal(significancia, 0.001))) {
+    rechaza <- "hay evidencia extremadamente fuerte de que H0 no es verdadera"
+  }
+  if (cola == 1) {
+    # dos colas
+    p <- pchisq(q = p_chisq, df = gl, lower.tail = FALSE) # saca el valor cola a la derecha y luego por dos
+    p.valor = 2 * p
+  }
+  
+  if (cola == 2) {
+    # dos colas
+    p <- pchisq(q = p_chisq, df = gl)
+    p.valor = p
+  }
+  if (cola == 3) {
+    # dos colas
+    p <- pchisq(q = p_chisq, df = gl, lower.tail = FALSE)
+    p.valor = p
+  }
+  
+  if (p.valor < significancia ) {
+    decision = paste("Se rechaza Ho porque ", rechaza)
+  }
+  
+  lista = list(p.valor = p.valor, decision = decision)
+  
+  return(lista)
+}
+
+
+# Septiebre 2023
+# Función para visualizar densidad de chi cuadrada 
+# Dependiendo si es hipótesis a dos colas cola izquierda o cola derecha
+# pone linea punteada de color rojo con el valor de prueba p_chisq
+
+
+f_probar_hipotesis_chisq_curve <- function(p_chisq, confianza, n, cola = 1) {
+  gl <- (n-1)
+  if (cola == 1) { # dos colas
+    alfa <- (1 - confianza) / 2
+    chisq_critico_izq = qchisq(p = alfa, df = gl)
+    chisq_critico_der = qchisq(p = alfa, df = gl, lower.tail = FALSE)
+    
+    # Visualizar con curve a dos colas
+    x <- 0:(chisq_critico_der + 10)
+    min <- min(x)
+    max <- max(x)
+    # Graficar la densidad de la distribución chi cuadrada
+    curve(dchisq(x, df = gl), from = min, to = max,
+          main = paste("Valores críticos y valor de prueba chisq"), 
+          sub=paste("Crítico izq=", round(chisq_critico_izq, 2), ";", "der=", round(chisq_critico_der, 2), "; gl=", gl),
+          ylab = 'Densidad', lwd = 2, col = 'steelblue') 
+    
+    # Izquierda
+    # Crear vector de valores izquierda x1 para dos colas
+    x_vector_i <- seq(from = min, to = chisq_critico_izq, by = 0.01)
+    
+    # Crear vector de valores de densidad chi-cuadrado para el intervalo
+    p_vector_i <- dchisq(x_vector_i, df = gl)
+    
+    # Rellenar el área bajo la curva del intervalo de confianza
+    
+    polygon(x = c(min, x_vector_i, chisq_critico_izq), y = c(min, p_vector_i, min), col = "orange", border = 'red')
+    
+    
+    # Derecha
+    # Crear vector de valores izquierda x1 para dos colas
+    x_vector_d <- seq(from = chisq_critico_der, to = max, by = 0.01)
+    
+    # Crear vector de valores de densidad chi-cuadrado para el intervalo
+    p_vector_d <- dchisq(x_vector_d, df = gl)
+    
+    # Rellenar el área bajo la curva del intervalo de confianza
+    polygon(x = c(chisq_critico_der, x_vector_d, max), y = c(min, p_vector_d, min), col = "orange", border = 'red') 
+    
+    abline(v = p_chisq, col='red', lwd = 1, lty= 4)
+
+  }
+  
+  if (cola == 2) { # cola izquierda
+    alfa <- (1 - confianza) 
+    chisq_critico_izq = qchisq(p = alfa, df = gl)
+    #chisq_critico_der = qchisq(p = 1 - alfa, df = gl)
+    
+    # VisualiZar con curve a dos colas
+    x <- 0:(chisq_critico_izq + 3 * chisq_critico_izq)
+    min <- min(x)
+    max <- max(x)
+    # Graficar la densidad de la distribución chi cuadrada
+    curve(dchisq(x, df = gl), from = min, to = max,
+          main = paste("Valor crítico y valor de prueba chisq"), 
+          sub=paste("Crítico izq=", round(chisq_critico_izq, 2), "; gl=", gl),
+          ylab = 'Densidad', lwd = 2, col = 'steelblue') 
+    
+    # Izquierda
+    # Crear vector de valores izquierda x1 para dos colas
+    x_vector_i <- seq(from = min, to = chisq_critico_izq, by = 0.01)
+    
+    # Crear vector de valores de densidad chi-cuadrado para el intervalo
+    p_vector_i <- dchisq(x_vector_i, df = gl)
+    
+    # Rellenar el área bajo la curva del intervalo de confianza
+    
+    polygon(x = c(min, x_vector_i, chisq_critico_izq), y = c(min, p_vector_i, min), col = "orange", border = 'red')
+    
+    
+    # Derecha
+    # Crear vector de valores izquierda x1 para dos colas
+  #  x_vector_d <- seq(from = chisq_critico_der, to = max, by = 0.01)
+    
+    # Crear vector de valores de densidad chi-cuadrado para el intervalo
+  #  p_vector_d <- dchisq(x_vector_d, df = gl)
+    
+    # Rellenar el área bajo la curva del intervalo de confianza
+  #  polygon(x = c(chisq_critico_der, x_vector_d, max), y = c(min, p_vector_d, min), col = "orange", border = 'red') 
+    
+    abline(v = p_chisq, col='red', lwd = 1, lty= 4)
+    
+  }
+  if (cola == 3) { # cola derecha
+    alfa <- (1 - confianza) 
+    # chisq_critico_izq = qchisq(p = alfa, df = gl)
+    chisq_critico_der = qchisq(p = alfa, df = gl, lower.tail = FALSE)
+    
+    # Visualiaar con curve cola derecha
+    x <- 0:(chisq_critico_der + 10)
+    min <- min(x)
+    max <- max(x)
+    # Graficar la densidad de la distribución chi cuadrada
+    curve(dchisq(x, df = gl), from = min, to = max,
+          main = paste("Valor crítico y valor de prueba chisq"), 
+          sub=paste("Critico der=", round(chisq_critico_der, 2), "; gl=", gl),
+          ylab = 'Densidad', lwd = 2, col = 'steelblue') 
+    
+    # Izquierda
+    # Crear vector de valores izquierda x1 para dos colas
+    #x_vector_i <- seq(from = min, to = chisq_critico_izq, by = 0.01)
+    
+    # Crear vector de valores de densidad chi-cuadrado para el intervalo
+    #p_vector_i <- dchisq(x_vector_i, df = gl)
+    
+    # Rellenar el área bajo la curva del intervalo de confianza
+    #polygon(x = c(min, x_vector_i, chisq_critico_izq), y = c(min, p_vector_i, min), col = "orange", border = 'red')
+    
+    
+    # Derecha
+    # Crear vector de valores izquierda x1 para dos colas
+    x_vector_d <- seq(from = chisq_critico_der, to = max, by = 0.01)
+    
+    # Crear vector de valores de densidad chi-cuadrado para el intervalo
+    p_vector_d <- dchisq(x_vector_d, df = gl)
+    
+    # Rellenar el área bajo la curva del intervalo de confianza
+    polygon(x = c(chisq_critico_der, x_vector_d, max), y = c(min, p_vector_d, min), col = "orange", border = 'red') 
+    
+    abline(v = p_chisq, col='red', lwd = 1, lty= 4)
+    
+  }
+  
+  
+}
+
+
 
 
